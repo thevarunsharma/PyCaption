cbba18f553fd5c6a17d3027768506cb579df8f16
Beast <varunsharma123.del@gmail.com>
2018-12-20 00:01:18 +0530

    minor improvements

--- bot.py
+++ bot.py
@@ -2,7 +2,6 @@ import requests
 import json
 import shutil
 from time import sleep
-import re
 import mlcode
 
 token = "742399582:AAHY_IvizrRsmjAnLkv_bCcxtm8kHbaYl2g"
@@ -12,21 +11,27 @@ def get_updates(offset = None):
     while True:
         url = baseurl + 'getUpdates'
         if offset:
-            url += '?offset={}'.format(offset) 
+            url += '?offset={}'.format(offset)
         res = requests.get(url)
         while (res.status_code !=200 or len(res.json()['result'])== 0):
             sleep(1)
             res = requests.get(url)
         print(res.url)
         return res.json()
-    
+
 def last_update_id(res):
     return res['result'][-1]['update_id']
+
+def is_starter(msg):
+    starter = ["hey", "hi", "hello", "hy", "start", "strt", "begin", "init"]
+    for s in starter:
+        if s in msg: return True
+    else: return False
 
 def get_start_chats(updates):
     start_chats = []
     for i in updates['result']:
-        if 'text' in i['message'] and re.search("start",i['message']['text'].lower()):
+        if 'text' in i['message'] and is_starter(i['message']['text'].lower()):
             start_chats.append(i['message']['chat']['id'])
             print("Starting chat with", i['message']['chat']['first_name'])
     return start_chats
@@ -46,16 +51,21 @@ def send_start_msg(chats):
 I am PyCaption Bot and I will help you decide a caption for your images.
 Send me an image and I will suggest a caption for it"""
     for cid in chats:
-        msg = requests.get(url, params={'chat_id':cid, 'text':text})
-        while msg.status_code!=200:
-            sleep(1)
-            msg = requests.get(url, params={'chat_id':cid, 'text':text})
+        while True:
+            try:
+                msg = requests.get(url, params={'chat_id':cid, 'text':text})
+                while msg.status_code!=200:
+                    sleep(1)
+                    msg = requests.get(url, params={'chat_id':cid, 'text':text})
+                break
+            except: pass
+        print('Responded with start message')
     return 'ok'
 
 def get_save_file(file_id):
     fileurl = baseurl + 'getFile'
     dwnurl = 'https://api.telegram.org/file/bot{}/'.format(token)
-    
+
     fl = requests.get(fileurl, params={'file_id':file_id})
     path = fl.json()['result']['file_path']
     dwn = requests.get(dwnurl+"{}".format(path))
@@ -64,28 +74,41 @@ def get_save_file(file_id):
         fl = requests.get(fileurl, params={'file_id':file_id})
         path = fl.json()['result']['file_path']
         dwn = requests.get(dwnurl+"{}".format(path))
-        
+
     with open("./images/"+file_id+".jpg", 'wb') as img:
             for chunk in dwn: img.write(chunk)
-                
+
 def send_photo_reply(photo_chats):
     sendurl = baseurl + 'sendMessage'
+    msg = "Image recieved!\nThinking of a caption..."
     for (chat_id, file_id) in photo_chats:
-        msg = "Image recieved!\nThinking of a caption..."
-        send = requests.get(sendurl, params={'chat_id':chat_id, 'text':msg})
-        while send.status_code!=200:
-            send = requests.get(sendurl, params={'chat_id':chat_id, 'text':msg})
-        get_save_file(file_id)
-    
+        while True:
+            try:
+                sleep(1)
+                send = requests.get(sendurl, params={'chat_id':chat_id, 'text':msg})
+                while send.status_code!=200:
+                    sleep(1)
+                    send = requests.get(sendurl, params={'chat_id':chat_id, 'text':msg})
+                get_save_file(file_id)
+                break
+            except: pass
+
     for (chat_id, file_id) in photo_chats:
         fname = "./images/{}.jpg".format(file_id)
         caption = mlcode.apply_model_to_image(fname).capitalize()
         text = "Here's the best one I can come up with\n\n\"{}\"".format(caption)
-        send = requests.get(sendurl, params={'chat_id':chat_id, 
+        while True:
+            try:
+                sleep(1)
+                send = requests.get(sendurl, params={'chat_id':chat_id,
                                         'text':text})
-        while send.status_code!=200:
-            send = requests.get(sendurl, params={'chat_id':chat_id, 
-                                        'text':text})
+                while send.status_code!=200:
+                    sleep(1)
+                    send = requests.get(sendurl, params={'chat_id':chat_id,
+                                        	'text':text})
+                break
+            except: pass
+        print('Responded with caption')
     return 'ok'
 
 def run():
@@ -100,5 +123,5 @@ def run():
             send_photo_reply(photo_chats)
         except KeyboardInterrupt:
             break
-            
+
 run()
